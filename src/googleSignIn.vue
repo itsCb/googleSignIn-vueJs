ab9a18898b65f84d1f354dc05759dc41422cec32
cb <chandrabharadanc@gmail.com>
2019-12-23 01:29:07 +0530

    Custom Button with slot usage is added

--- src/googleSignIn.vue
+++ src/googleSignIn.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="googleSignIn" :class="typeof className !== 'undefined' ? className : null"></div>
+  <div id="googleSignIn" :class="typeof className !== 'undefined' ? className : null">
+      <slot v-if="customButton"></slot>
+  </div>
 </template>
 
 <script>
@@ -20,6 +22,10 @@
       customButton:{
         default:false,
         type:Boolean
+      },
+      customButtonId:{
+        Default:null,
+        type:String
       },
       longTitle:{
         default:false,
@@ -43,26 +49,43 @@
       },
     },
     mounted() {
-      window.__google_oauth_useful_variables = {client_id: this.clientId, warning: this.warning};
-      gapi.load('auth2', function(){
-        try {
-          auth2 = gapi.auth2.init({
-            client_id: window.__google_oauth_useful_variables.client_id,
-            cookiepolicy: 'single_host_origin',
-          });
-        }catch (e) {
-          window.__google_oauth_useful_variables.warning ? console.warn(e):null;
+        window.__google_oauth_useful_variables = {
+            client_id: this.clientId,
+            warning: this.warning,
+            customButtonId: this.customButtonId,
+            attachSignin: this.attachSignin,
+            onSignIn:this.onSignIn,
+            onFailSignIn:this.onFailSignIn
+        };
+        gapi.load('auth2', function(){
+            try {
+                window.auth2 = gapi.auth2.init({
+                    client_id: window.__google_oauth_useful_variables.client_id,
+                    cookiepolicy: 'single_host_origin',
+                });
+            }catch (e) {
+                window.__google_oauth_useful_variables.warning ? console.warn(e):null;
+            }
+            try {
+                if (window.__google_oauth_useful_variables.customButtonId){
+                    window.__google_oauth_useful_variables.attachSignin(document.getElementById(window.__google_oauth_useful_variables.customButtonId));
+                }
+            }catch (e) {
+                window.__google_oauth_useful_variables.warning ? console.warn(e):null;
+            }
+        });
+
+        if(!this.customButton) {
+            gapi.signin2.render('googleSignIn', {
+                scope: this.scope,
+                width: this.width,
+                height: this.height,
+                longTitle: this.longTitle,
+                theme: this.theme,
+                onsuccess: this.onSignIn,
+                onfailure: this.onFailSignIn
+            });
         }
-      });
-      gapi.signin2.render('googleSignIn', {
-        scope:this.scope,
-        width:this.width,
-        height:this.height,
-        longTitle:this.longTitle,
-        theme:this.theme,
-        onsuccess: this.onSignIn,
-        onfailure: this.onFailSignIn
-      });
       this.validate();
     },
     methods:{
@@ -75,7 +98,21 @@
           this.warning ? console.warn("Google Sign In --> Specify the Failure callback method to get the Error data") : null;
         else if(typeof this.failureCallBack !== "function")
           this.warning ? console.warn("Google Sign In --> Failure Callback must be a valid Function") : null;
+        if(this.customButton) {
+            if(typeof this.customButtonId === 'undefined')
+                this.warning ? console.warn("Google Sign In --> Please Provide customButtonId or change the customButton to false") : null;
+
+        }
       },
+        attachSignin: function (element) {
+            window.auth2.attachClickHandler(element, {},
+                function(googleUser) {
+                    window.__google_oauth_useful_variables.onSignIn(googleUser);
+                }, function(error) {
+                    window.__google_oauth_useful_variables.onFailSignIn(error);
+                }
+            );
+        },
       onSignIn: function (googleUser) {
         let id_token = googleUser.getAuthResponse().id_token;
         let profile = googleUser.getBasicProfile();
